bc300c3597976c316cc242d58670aff821702e8f
Eliane Lell <lelltchatchoua@et.esiea.fr>
2024-12-18 11:56:35 +0100

    initial streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+
+
+
+st.header("Welcome to this course!!!", divider=True)
+
+
+
+st.subheader("Introduction to DevOps")
+
+
+
+st.write ("Hello MBADIA Students !")
+
+
+
+st.text("This course is short introduction to devops !!!")
